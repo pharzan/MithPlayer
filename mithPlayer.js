f1c7cbf0178f2d0bfb7fd6e2133ba75684fb0eb6
Farzan <pharzan@outlook.com>
2015-10-12 20:16:27 +0300

    New Version

--- mithPlayer.js
+++ mithPlayer.js
@@ -1,144 +1,185 @@
-//var myPlayer = thePlayerName;
+/*
+ The Player
+ */
 var Player = function () {
 
     this.config = {
-        theVideo: m.prop(),
-        theDims: m.prop({w: 400}),
-        theUrl: m.prop("http://media.w3.org/2010/05/bunny/trailer.mp4"),
-        thePlugins: []
+        dimensions: {w: "400px", h: "225px"},
+        autoPlay: true,
+        playList: [
+            'file://C:/wamp/www/mithPlayer/videos/trailer.mp4',
+            'file://C:/wamp/www/mithPlayer/videos/bunny.mp4',
+            'file://C:/wamp/www/mithPlayer/videos/starwars.mp4'
+        ],
+        loop: true
     };
 
-// this function is
-    this.delayFunction = function () {
-        var self = this;
-        var deferred = m.deferred();
-        setTimeout(function () {
-            var theVid = self.config.theVideo();
-            deferred.resolve(theVid);
-        }, 2);
-        return deferred.promise;
+    this.state = {
+        playing: false,
+        finished: false,
+        fileIdx: 0,
+        time:0
+    };
+
+    this.thePlayerObj = m.prop();
+
+    this.timeUpdate = function (seekBarId) {
+        m.startComputation();
+        this.state.time=this.thePlayerObj().currentTime;
+        m.endComputation();
+
     };
 
     this.playIt = function () {
-        this.delayFunction()
-            .then(function (theVid) {
-                console.log(theVid);
-                if (theVid.paused) {
-                    theVid.play()
-                } else {
-                    theVid.pause()
-                }
-            })
+        var me = this.thePlayerObj();
+        var self = this;
 
+        if (me.paused) {
+            this.state.playing = true;
+            me.play();
+            seekBarId = setInterval(function () {
 
-
-    };
-
-    this.loadVideo = function (videoUrl) {
-
-        if (videoUrl) {
-            this.config.theUrl(videoUrl)
+                self.timeUpdate(seekBarId);
+            }, 1);
         } else {
-            //default video if nothing is passed also can add a try exception
-            //here to check for video availibility
-            this.config.theUrl('http://media.w3.org/2010/05/bunny/trailer.mp4')
-        }
+            clearInterval(seekBarId);
+            this.state.playing = false;
+            me.pause();
 
-        var theVid = this.config.theVideo();
-//change the src of source not video as set in the tag
-        theVid.src = this.config.theUrl();
-        theVid.load();
-        console.log('URL', videoUrl)
-    };
 
-    this.skipVideo = function (value) {
-        var theVid = this.config.theVideo();
-        console.log(this.config.theVideo())
-        theVid.currentTime += value;
-    };
-
-    this.volumeVideo = function (vol) {
-        var theVid = this.config.theVideo();
-        if (theVid.volume + vol >= 0 && theVid.volume + vol < 1) {
-            theVid.volume = theVid.volume + vol
         }
     };
 
+    this.loadFromPlayList = function () {
+
+        var me = this.thePlayerObj();
+        var config = this.config;
+        var fileIdx = this.state.fileIdx;
+        this.state.fileIdx++;
+
+        if (config.playList[fileIdx]) {
+            me.src = config.playList[fileIdx];
+            me.load();
+
+        }
+        else if (config.loop && fileIdx == config.playList.length) {
+            this.state.fileIdx = 0;
+            this.loadFromPlayList()
+        }
+
+    }.bind(this);
     this.view = function () {
         var self = this;
-        var videoView = [m("video",
-            {
-                controls: "controls",
-                height: this.config.theDims().h,
-                width: this.config.theDims().w,
-                config: function (element, isinit) {
-                    self.config.theVideo(element);
-                }
-            },
-            m("source",
+        var videoView = [
+            m("video",
                 {
-                    //default should be somewhere else not here
-                    src: "http://media.w3.org/2010/05/bunny/trailer.mp4",
-                    type: "video/mp4"
-                })
-        ), m('div', {onclick: this.skipVideo.bind(this, 0.5)}, 'SKIP')];
-        var volView = m('div', {
-            onclick: this.volumeVideo.bind(this, 0.1)
-        }, 'Vol +');
-        var theView = [videoView];
-        theView.push(volView, m.component(theOverlays, this));
+                    controls: 'controls',
+                    height: self.config.dimensions.h,
+                    width: self.config.dimensions.w,
+                    oncanplay: function () {
+                        if (self.config.autoPlay) {
+                            self.playIt()
+                        }
+                    },
+                    config: function (element, isinit) {
+                        if (isinit) {
+                            return
+                        }
+                        self.thePlayerObj(element);
 
-        return [m(".video_container", {}, theView)];
+                        self.loadFromPlayList()
 
-    };
+                    },
+                    onended: self.loadFromPlayList
 
+                },
+                m("source",
+                    {
+                        src: '',
+                        type: "video/mp4"
+                    })
+            )
+        ];
+
+        videoView.push(m.component(overlayControls, self));
+
+        return [m('.videoContainer', {
+
+            height: self.config.dimensions.h,
+            width: self.config.dimensions.w,
+            style: {
+                position: "relative",
+                width: self.config.dimensions.w,
+                height: self.config.dimensions.h
+            }
+        }, videoView)]
+    }
 };
-var theOverlays = {
 
-    vm: {
-        self: m.prop(this)
+/*
+ The custom overlay for the controls:
+ */
+var overlayControls = {
+
+    overlayPlay: function (parent) {
+
+        parent.playIt()
     },
-    playIt: function () {
-        this.vm.self().style.display = "none";
-        console.log(this);
-        this.playIt();
-
-    },
-
     controller: function () {
     },
     view: function (ctrl, parent) {
         var self = this;
-        console.log("parent:", parent);
-        return m("div", [
-            m("img", {
-                src: 'assets/img/play_button.png',
-                style: {
-                    position: "absolute",
-                    top: "0"
-                },
-                onclick: function(){
-                    console.log(this)
-                    parent.playIt.bind(parent)
-                },
-                config: function (element, isinit) {
-                    self.vm.self(element)
+
+        this.controlsConfig = {
+            playUrl: "url('file://C:/wamp/www/mithPlayer/assets/img/playBtn.png')",
+            playDimensions: {h: "100px", w: "100px"},
+            pauseUrl: "url('file://C:/wamp/www/mithPlayer/assets/img/pauseBtn.png')",
+            pauseDimensions: {h: "25px", w: "25px"}
+        };
+
+        return m('btns',
+            m("div.playBtn",
+                {
+                    width: parent.config.dimensions.w,
+                    height: parent.config.dimensions.h,
+                    style: {
+                        position: "absolute",
+                        left: "35%",
+                        top: "20%",
+                        color: "red",
+                        height: this.controlsConfig.playDimensions.h,
+                        width: this.controlsConfig.playDimensions.w,
+                        backgroundImage: this.controlsConfig.playUrl,
+                        backgroundRepeat: "no-repeat",
+                        display: parent.state.playing ? "none" : "block",
+                        backgroundSize: "cover"
+                    },
+                    onclick: function () {
+                        self.overlayPlay(parent)
+                    }
                 }
-            })
-        ])
+            ),
+            m('.pauseBtn', {
+                    style: {
+                        opacity: .5,
+                        position: "absolute",
+                        left: "5px",
+                        bottom: "5px",
+                        color: "red",
+                        height: this.controlsConfig.pauseDimensions.h,
+                        width: this.controlsConfig.pauseDimensions.w,
+                        backgroundImage: this.controlsConfig.pauseUrl,
+                        backgroundRepeat: "no-repeat",
+                        display: parent.state.playing ? "block" : "none",
+                        backgroundSize: "cover"
+                    },
+                    onclick: parent.playIt.bind(parent)
+                }
+            ),
+            m('.seek',{onclick:function(){console.log(parent.thePlayerObj().currentTime)}},parent.state.time)
+        )
     }
 };
 
 myPlayer = new Player();
-
-
-//myPlayer.playIt();
 m.mount(document.body, myPlayer);
-/*
- different sources : change files
- access controls using api myPlayer.play() ....
- add some plugins : overlay div
-
-
-
- */
